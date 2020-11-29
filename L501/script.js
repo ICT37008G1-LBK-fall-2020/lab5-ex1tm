9fc6a565008619328da896b836492fa906167aa8
ex1tm <misho0318@mail.ru>
2020-11-29 20:10:36 +0400

    1

---
+++ L501/script.js
@@ -0,0 +1,11 @@
+// 1.1
+console.log(document.head);
+alert('head ელემენტი:\n\n'+document.head);
+
+// 1.2
+console.log(document.getElementsByTagName('ul')[0]);
+alert('ul ელემენტი:\n\n'+document.getElementsByTagName('ul')[0]);
+
+// 1.3
+console.log(document.getElementsByTagName('li')[1]);
+alert('მეორე li ელემენტი:\n\n'+document.getElementsByTagName('li')[1]); 
